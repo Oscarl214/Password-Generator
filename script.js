1e91257f26c8d34e841f0ed848438c45e14e5e93
Oscar Leal <oscarleal234@gmail.com>
2023-02-18 21:56:18 -0600

    broken down psuedo code and first steps

--- script.js
+++ script.js
@@ -60,9 +60,34 @@ var specialC = ["!", "#", "$", "%", "&", "'", "*", "+", ",", "-", ".", "/"];
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Hey you clicked the button"); //make this a habit to make sure when you click a button you created that stuff is connected the way they should be connected
+
+  //1. Prompt the user for the password criteria
+  //    a. password length 8<128
+  //    b. types: lowercase, uppercase, numeric, or special
+  //2. Validate the input. Maker sure password length is 8>128, and that at least one type is selected
+  //3. Generate password based on criteria
+
+  var passwordLength = window.prompt(
+    "How long do you want your password to be?(8 min- 128 max)"
+  );
+  var passwordLowercase = window.prompt(
+    "Do you you want your password to include lowercase letters?(Press Cancel for No)"
+  );
+  var passwordUppercase = window.prompt(
+    "Do you want your password to include uppercase letters?(Press Cancel for No)"
+  );
+  var passwordSpecial = window.prompt(
+    "Do you want your password to include special characters?(Press Cancel for No)"
+  );
+  //4. Display password on the page
+  return "Generated Password"; //write my code and return my generated password which I will create as a variable
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); //generatePassword is what I need to create
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
