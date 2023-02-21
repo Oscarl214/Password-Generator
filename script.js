d08f9711ef4955d221b3d810ddeeb80da9581187
Oscar Leal <oscarleal234@gmail.com>
2023-02-20 21:33:17 -0600

    new comments

--- script.js
+++ script.js
@@ -5,20 +5,10 @@ var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-var numbers = "0123456789".split(""); // .split method seperates string into arrays
+var numbers = "0123456789".split(""); // .split method seperate string into arrays
 var specialC = "!@#$%^&*()_".split("");
 
 function generatePassword() {
-  //My function that gets called on to run the password generator
-  // console.log("Hey you clicked the button"); //made this a habit to make sure when I click a button, stuff is connected by confirming via the console
-
-  //Psuedo Code:
-  //1. Prompt the user for the password criteria
-  //    a. password length 8<128
-  //    b. types: lowercase, uppercase, numeric, or special
-  //2. Validate the input. Maker sure password length is 8>128, and that at least one type is selected
-  //3. Generate password based on criteria
-
   var passwordLength = ""; //my password length variable which I pass on an empty string value bc it will contain a new value once user selects how long they want their password to be
   var passwordLength = prompt(
     //Prompts the user on how many characters they want
@@ -26,17 +16,22 @@ function generatePassword() {
   );
 
   //Message shown when employee selects wrong min or wrong max character length
-  while (passwordLength <= 7 || passwordLength >= 129) {
+  while (
+    passwordLength <= 7 ||
+    passwordLength >= 129 ||
+    isNaN(passwordLength) //if User were to input anything that is not a number
+  ) {
     alert(
       "Password length must be between 8 - 128 characters. Please try again."
     );
+
     var passwordLength = prompt(
       "How many characters do you want your password to be? (8 - 128 characters)."
     );
   }
 
   //Message shown when password length is accepted
-  alert("Your password length will be " + passwordLength + " characters.");
+  alert(`Your password length will be ${passwordLength}`);
 
   //Ask user what characters should be use
 
@@ -80,19 +75,19 @@ function generatePassword() {
   //Empty array that is created which will gain values once criteria is determined
   var selectedArray = [];
 
-  if (addNumbers === true) {
+  if (addNumbers) {
     selectedArray = selectedArray.concat(numbers); //concat method allows me to merge two or more arrays
   } // pass on the number characters to the empty selected array if it is true/selected
 
-  if (addUpper === true) {
+  if (addUpper) {
     selectedArray = selectedArray.concat(upperCase); //For each array I have to recall the selectedArray because If I don't the concat method will only output the original empty array
   } // pass on the upperCase characters to the selected array with number characters if it is true/selected
 
-  if (addLower === true) {
+  if (addLower) {
     selectedArray = selectedArray.concat(lowerCase);
   } // pass on the lower case characters to the selected array with numbers & Upper case characters if it is true/selected
 
-  if (addSpecial === true) {
+  if (addSpecial) {
     selectedArray = selectedArray.concat(specialC);
   } // pass on the special characters to the  selected array that contains all other selected criteria if it is true/selected
 
@@ -105,6 +100,9 @@ function generatePassword() {
       randomPassword +
       selectedArray[Math.floor(Math.random() * selectedArray.length)]; //applies the randomized value of selected criteria to the empty random Password variable I created
   }
+
+  //selectedArray(set of characters)[index of selectedArray.length * random argument]
+
   //4. Display password on the page
   return randomPassword; //returns the random/Users Password
 }
@@ -119,3 +117,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Psuedo Code:
+//1. Prompt the user for the password criteria
+//    a. password length 8<128
+//    b. types: lowercase, uppercase, numeric, or special
+//2. Validate the input. Maker sure password length is 8>128, and that at least one type is selected
+//3. Generate password based on criteria
